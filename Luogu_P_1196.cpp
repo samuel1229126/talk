045cc360bbf5f7d0ab91497dd0ff83d268d225fe
Samuel <samuel1229@126.com>
2025-09-29 13:45:11 +0800

    commit

--- Luogu_P_1196.cpp
+++ Luogu_P_1196.cpp
@@ -1,34 +1,44 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int fa[30001],front[30001],num[30001],x,y,i,j,n,T,ans;
+
+int fa[30001], front[30001], num[30001], x, y, i, j, T, ans;
 char ins;
-int find(int n){
-    if(fa[n]==n)return fa[n];
-    int fn=find(fa[n]);
-    front[n]+=front[fa[n]];
-    return fa[n]=fn;
+
+int find(int n) {
+    if (fa[n] == n) return fa[n];
+    int fn = find(fa[n]);
+    front[n] += front[fa[n]];
+    return fa[n] = fn;
 }
-int main(){
-    cin>>T;
-    for(i=1;i<=30000;++i){
-        fa[i]=i;
-        front[i]=0;
-        num[i]=1;
+
+int main() {
+    cin >> T;
+    for (i = 1; i <= 30000; ++i) {
+        fa[i] = i;
+        front[i] = 0;
+        num[i] = 1;
     }
-    while(T--){
-        cin>>ins>>x>>y;
-        int fx=find(x);
-        int fy=find(y);
-        if(ins=='M'){
-            front[fx]+=num[fy];
-            fa[fx]=fy;
-            num[fy]+=num[fx];
-            num[fx]=0;
+
+    while (T--) {
+        cin >> ins >> x >> y;
+        int fx = find(x);
+        int fy = find(y);
+
+        if (ins == 'M') {
+            front[fx] += num[fy];
+            fa[fx] = fy;
+            num[fy] += num[fx];
+            num[fx] = 0;
         }
-        if(ins=='C'){
-            if(fx!=fy)cout<<"-1"<<endl;
-            else cout<<abs(front[x]-front[y])-1<<endl;
+
+        if (ins == 'C') {
+            if (fx != fy) {
+                cout << "-1" << endl;
+            } else {
+                cout << abs(front[x] - front[y]) - 1 << endl;
+            }
         }
     }
+
     return 0;
 }
